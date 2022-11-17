395f4814e22aca6e38352fac2d79c43610806602
Mahalakshmi-1407 <118158211+Mahalakshmi-1407@users.noreply.github.com>
2022-11-17 18:23:34 +0530

    Add files via upload

---
+++ Assignment-1/qn-9.sh
@@ -0,0 +1,10 @@
+crontab -l
+crontab -e
+# enter below command
+#    * * * * * /bin/sh /home/maha/Desktop/Assignment-1/script.sh
+#    * * * * * sleep 30; /bin/sh /home/maha/Desktop/Assignment-1/script.sh
+#write below command in script.sh
+touch /home/maha/Desktop/Assignment-1/file.txt
+ls -l
+
+
